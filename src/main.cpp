dd0d69cf05a0cf942ef3f1bbd625655d536b2e9a
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-11-18 18:18:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ double make_fib(size_t n) {
 	return make_fib(n-1) + make_fib(n-2);
 }
 
-//ôíêöèÿ äëÿ çàïîëíåíèÿ è âûâîäà íà ýêðàí êîíòåéíåðà std::map
+
 template <typename T>
 void make_map(T& cont, size_t n) {
 	//çàïîëíåíèå êîíòåéíåðà çíà÷åíèÿìè
@@ -47,10 +47,10 @@ void make_map(T& cont, size_t n) {
 	}
 }
 
-//ôíêöèÿ äëÿ çàïîëíåíèÿ è âûâîäà íà ýêðàí êîíòåéíåðà my_cont
+
 template <typename T>
 void make_cont(T& cont, size_t n) {
-	//çàïîëíåíèå êîíòåéíåðà çíà÷åíèÿìè
+	
 	for (size_t i = 0; i < n; ++i) {
 		cont.enqueue(make_fac(i), make_fib(i));
 		std::cout << "---" << std::endl;
@@ -60,24 +60,6 @@ void make_cont(T& cont, size_t n) {
 
 
 int main(int, char* []) {
-	/*
-	auto v = std::vector<int, logging_allocator<int>>{};
-	    v.reserve(5);
-	for (size_t i = 0; i < 5; ++i) {
-		v.emplace_back(i);
-		std::cout << "---" << std::endl;
-	}
-
-	for (auto i : v) {
-		std::cout << i << std::endl;
-	}
-
-	auto h = std::map<int, hard, std::less<int>, logging_allocator<std::pair<const int, hard>>>{};
-		for (size_t i = 0; i < 5; ++i) {
-			h.emplace(i, hard(i, i));
-			std::cout <<"---" << std::endl;
-		}
-*/	
 
 	auto m_std = std::map<int, hard>{};
 	auto m_all = std::map<int, hard, std::less<int>, my_allocator<std::pair<const int, hard>, 10>>{};
@@ -98,95 +80,9 @@ int main(int, char* []) {
 	for (auto& i : q_all) {
 		std::cout << i.i << " " << i.d << std::endl;
 	}
-	/*
-	std::cout << "m_std---" << std::endl;
-	for (size_t i = 1; i < 12; ++i) {
-	        m_std.emplace(i, hard(make_fac(i), make_fib(i)));
-	        std::cout << "---" << std::endl;
-	}
 
-	for (auto i : m_std) {
-			std::cout << i.second.i << " " << i.second.d << std::endl;
-	}
-	
-	std::cout << "m_all---" << std::endl;
-
-	for (size_t i = 1; i < 12; ++i) {
-		m_std.emplace(i, hard(make_fac(i), make_fib(i)));
-		std::cout << "---" << std::endl;
-	}
-
-	for (auto i : m_all) {
-		std::cout << i.second.i << " " << i.second.d << std::endl;
-	}
-	
-	//auto q = Queue<hard, my_allocator<Node<hard>, 10>>{};
-	
-	
-	for (int i = 0; i < 12; ++i) {
-		q_std.enqueue(i, i);
-		std::cout << "---" << std::endl;
-	}
-
-	for (auto& i : q_std) {
-		std::cout << i.i << " " << i.d << std::endl;
-	}
-	
-	
-	for (size_t i = 0; i < 12; ++i) {
-		q_all.enqueue(make_fac(i), make_fib(i));
-		std::cout << "---" << std::endl;
-	}
-
-	for (auto & i : q_all) {
-		std::cout << i.i << " " << i.d << std::endl;
-	}
-	*/
 
 	return 0;
 }
 
-/*
-struct hard {
-	int i;
-	double d;
 
-	hard(int i, double d) : i(i), d(d) { std::cout << __PRETTY_FUNCTION__ << std::endl; };
-
-	hard(const hard &)
-	= delete;
-//    { std::cout << __PRETTY_FUNCTION__ << std::endl; };
-
-	hard(hard &&) noexcept
-	= delete;
-//    { std::cout << __PRETTY_FUNCTION__ << std::endl; };
-
-	~hard() { std::cout << __PRETTY_FUNCTION__ << std::endl; };
-};
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-h.emplace(std::piecewise_construct, std::forward_as_tuple(i), std::forward_as_tuple(i, i));
-*/
