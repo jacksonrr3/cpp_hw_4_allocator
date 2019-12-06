c5fd4fab335db99964c7ee30558a8d212b49853f
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-12-06 14:45:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -55,16 +55,16 @@ void make_cont(T& cont, size_t n) {
 
 int main(int, char* []) {
 
-	auto m_std = std::map<int, hard>{};
-	auto m_all = std::map<int, hard, std::less<int>, my_allocator<std::pair<const int, hard>, 10>>{};
+//	auto m_std = std::map<int, hard>{};
+//	auto m_all = std::map<int, hard, std::less<int>, my_allocator<std::pair<const int, hard>, 10>>{};
 	auto q_std = Queue<hard>{};
-	auto q_all = Queue<hard, my_allocator<Node<hard>, 10>>{};
+//	auto q_all = Queue<hard, my_allocator<Node<hard>, 10>>{};
 
-	make_map(m_std, 11);
-	make_map(m_all, 11);
+//	make_map(m_std, 11);
+//	make_map(m_all, 11);
 	make_cont(q_std, 11);
-	make_cont(q_all, 11);
-
+//	make_cont(q_all, 11);
+/*
 	for (auto& i : m_all) {
 		std::cout << i.second.i << " " << i.second.d << std::endl;
 	}
@@ -72,34 +72,35 @@ int main(int, char* []) {
 	for (auto& i : q_all) {
 		std::cout << i.i << " " << i.d << std::endl;
 	}
-
+*/
+/*	
 	//дополнительный контейнер для проверки конструкторов копирования
 	//так как у структуры Hard удалены конструкторы копирования, используем int
 	auto q_all_1 = Queue<int, my_allocator<Node<int>, 10>>{};
 	for (size_t i = 0; i < 11; ++i) {
 		q_all_1.enqueue(i);
 	}
-
+*/
 	//тест работы конструктора копирования при совпадающих аллокаторах контейнера
-	Queue<int, my_allocator<Node<int>, 10>> q_all_2(q_all_1);
+//	Queue<int, my_allocator<Node<int>, 10>> q_all_2(q_all_1);
 	/*for (auto& i : q_all_2) {
 		std::cout << i << std::endl;   
 	}*/
 
 	//тест работы конструктора копирования при отличающихся аллокаторах контейнера
-	Queue<int, logging_allocator<Node<int>>> q_all_3(q_all_1);
+//	Queue<int, logging_allocator<Node<int>>> q_all_3(q_all_1);
 	/*for (auto& i : q_all_2) {
 		std::cout << i << std::endl;
 	}*/
 
 	//тест работы конструктора копирования перемещением при совпадающих аллокаторах контейнера
-	Queue<int, my_allocator<Node<int>, 10>> q_all_4(std::move(q_all_2));
+//	Queue<int, my_allocator<Node<int>, 10>> q_all_4(std::move(q_all_2));
 	/*for (auto& i : q_all_4) {
 		std::cout << i << std::endl;
 	}*/
 
 	//тест работы конструктора копирования перемещением при совпадающих аллокаторах контейнера
-	Queue<int, my_allocator<Node<int>, 10>> q_all_5(std::move(q_all_3));
+//	Queue<int, my_allocator<Node<int>, 10>> q_all_5(std::move(q_all_3));
 	/*for (auto& i : q_all_5) {
 		std::cout << i << std::endl;
 	}*/
